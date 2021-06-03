2be8ff601c4ffb0c4673fbcca10eed3e0bfa5847
Captain <captain@Captainui-MacBookAir.local>
2021-06-03 22:31:28 +0900

    Proxy Setting

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 function LandingPage() {
+  
+  useEffect(() => {
+    axios.get('/api/hello')
+    .then(res => console.log(res));
+  }, [])
   return (
     <div>
       LandingPage
